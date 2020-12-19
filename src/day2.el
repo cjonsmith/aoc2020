d56521b955e1dd86c86d510107af015650598312
Cameron Smith <cjonsmith@gmail.com>
2020-12-19 14:55:27 -0800

    Solution to part 2 of day 2

--- src/day2.el
+++ src/day2.el
@@ -25,4 +25,26 @@
 	     (<= num-chars-in-pw max))
 	(setq count (1+ count)))))
   (message "%d" count))
+
+;; Part 2
+(let ((data (split-string
+	     (with-temp-buffer
+	       (insert-file-contents "../input/day2.txt")
+	       (buffer-string)) "\n" t))
+      (count 0))
+  (dolist (line data)
+    (let* ((split-line (split-string line))
+	   (indicies (mapcar 'string-to-number
+			     (split-string
+			      (nth 0 split-line)
+			      "-")))
+	   (index-a (- (nth 0 indicies) 1))
+	   (index-b (- (nth 1 indicies) 1))
+	   (character (string-to-char (substring (nth 1 split-line) 0 1)))
+	   (password (nth 2 split-line)))
+      (when (xor
+	     (char-equal (string-to-char (string (elt password index-a))) character)
+	     (char-equal (string-to-char (string (elt password index-b))) character))
+	(setq count (1+ count)))))
+  (message "%d" count))
 ;;; day2.el ends here
