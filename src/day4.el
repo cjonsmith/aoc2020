0b3caead3847b010bdc4bea68456f4edbce37cd7
Cameron Smith <cjonsmith@gmail.com>
2020-12-21 19:25:54 -0800

    Wrap all regular expressions in word boundaries.
    It seems that I didn't handle case where PIDs were longer than 9
    digits long. Wrapping all regular expressions in word boundaries to
    prevent more than what I'm looking to match from sneaking through the checks.

--- src/day4.el
+++ src/day4.el
@@ -1,7 +1,5 @@
 ;;; day4.el --- My solution to the fourth day of the 2020 Advent of Code
 ;;; Commentary:
-;;;   Part two of this solution is off by one and I don't really want to debug
-;;;   my regular expresssions.  I may come back to this problem later.
 ;;; Code:
 (defun no-move-search (string bound &optional regexp)
   "Search forward for STRING up to position BOUND without moving point.
@@ -22,18 +20,18 @@ If optional argument REGEXP is non-nil, then search using STRING as a regular ex
 
 (defun validate-passport (bound)
   "Determine if a passport defined up to position BOUND is valid."
-  (and (no-move-search "ecl:\\(amb\\|blu\\|brn\\|gry\\|grn\\|hzl\\|oth\\)" bound t)
-       (no-move-search "pid:[[:digit:]]\\{9\\}" bound t)
-       (no-move-search "eyr:\\(202[0-9]\\|2030\\)" bound t)
-       (no-move-search "hcl:#\\([[:digit:]]\\|[a-f]\\)\\{6\\}" bound t)
-       (no-move-search "byr:\\(19[2-9][[:digit:]]\\|200[0-2]\\)" bound t)
-       (no-move-search "iyr:\\(201[[:digit:]]\\|2020\\)" bound t)
-       (no-move-search "hgt:\\(1\\([5-8][[:digit:]]\\|9[0-3]\\)cm\\|\\(59\\|6[[:digit:]]\\|7[0-6]\\)in\\)" bound t)))
+  (and (no-move-search "\\becl:\\(amb\\|blu\\|brn\\|gry\\|grn\\|hzl\\|oth\\)\\b" bound t)
+       (no-move-search "\\bpid:[[:digit:]]\\{9\\}\\b" bound t)
+       (no-move-search "\\beyr:\\(202[0-9]\\|2030\\)\\b" bound t)
+       (no-move-search "\\bhcl:#\\([[:digit:]]\\|[a-f]\\)\\{6\\}\\b" bound t)
+       (no-move-search "\\bbyr:\\(19[2-9][[:digit:]]\\|200[0-2]\\)\\b" bound t)
+       (no-move-search "\\biyr:\\(201[[:digit:]]\\|2020\\)\\b" bound t)
+       (no-move-search "\\bhgt:\\(1\\([5-8][[:digit:]]\\|9[0-3]\\)cm\\|\\(59\\|6[[:digit:]]\\|7[0-6]\\)in\\)\\b" bound t)))
 
 (defun day4 (part)
   "Output the solution to part PART of the fourth day's problem."
   (with-temp-buffer
-    (insert-file-contents "../input/new-day4.txt")
+    (insert-file-contents "../input/day4.txt")
     (let ((count 0))
       (while (not (eobp))
 	(let ((end-of-passport-pos (no-move-search "^$" nil t)))
@@ -44,11 +42,8 @@ If optional argument REGEXP is non-nil, then search using STRING as a regular ex
 	      (setq count (1+ count))))
 	  (search-forward-regexp "^$" nil t)
 	  (forward-line)))
-      ;; Off by one; my regular expressions might be off somehow.  Was able to assume that
-      ;; I was close enough and just guessed one less than what I got.
-      (when (equal part 2)
-	(setq count (1- count)))
       count)))
 
+(message "%s" (day4 1))
 (message "%s" (day4 2))
 ;;; day4.el ends here
