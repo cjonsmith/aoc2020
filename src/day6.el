0af942398c7271acb49343ae4d170dacf46c9d08
Cameron Smith <cjonsmith@gmail.com>
2020-12-28 23:40:50 -0800

    Solution to part two of day six.

--- src/day6.el
+++ src/day6.el
@@ -1,6 +1,9 @@
 ;;; day6.el --- My solution to the sixth day of the 2020 Advent of Code
 ;;; Commentary:
 ;;; Code:
+(require 'cl-lib)
+
+;; Part 1
 (with-temp-buffer
   (insert-file-contents "../input/day6.txt")
   (let ((total-responses 0))
@@ -10,6 +13,22 @@
       (setq total-responses (+ total-responses
 			       (length (delete-dups (delq (string-to-char "\n")
 							  (append (buffer-string) ()))))))
+      (widen)
+      (forward-line))
+    total-responses))
+
+;; Part 2
+(with-temp-buffer
+  (insert-file-contents "../input/day6.txt")
+  (let ((total-responses 0))
+    (while (not (eobp))
+      (narrow-to-region (point)
+			(re-search-forward "^$"))
+      (let* ((responses (split-string (buffer-string)))
+	     (resp-intersect (append (car responses) ())))
+	(dolist (response (cdr responses))
+	  (setq resp-intersect (cl-intersection resp-intersect (append response ()))))
+	(setq total-responses (+ total-responses (length resp-intersect))))
       (widen)
       (forward-line))
     total-responses))
